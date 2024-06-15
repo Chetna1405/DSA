23240775832f85ef9774dfb9d6051fd65d0442c3
Chetna1405 <gargchetna11@gmail.com>
2024-06-15 13:02:08 +0530

    ..

--- PracticeSet/Arrays/ReversalAlgorithm.java
+++ PracticeSet/Arrays/ReversalAlgorithm.java
@@ -1,7 +1,7 @@
 package PracticeSet.Arrays;
 import java.util.* ;
 //Array Reversal Algorithm
-public class Array {
+public class ReversalAlgorithm {
    public static void main(String[] args) {
         int[] arr ={1,2,3,4,5,6};
         int k = 3 ;
--- PracticeSet/Sorting/bubbleSort.java
+++ PracticeSet/Sorting/bubbleSort.java
@@ -4,19 +4,14 @@ import java.util.Arrays;
 
 public class bubbleSort {
     public static void main(String[] args) {
-        int[] arr = {4,3,6,1,2};
+        int[] arr = {4,3,6,1,9};
         for(int i = 0 ;i < arr.length - 1 ; i++){
-            int p = i ;
-            if(arr[p] > arr[p+1]) {
-                while (p < arr.length && arr[p] > arr[p + 1]) {
-                    int temp = arr[p];
-                    arr[p] = arr[p + 1];
-                    arr[p + 1] = temp;
-                    System.out.println(arr[p]);
-                    p++;
-                }
-                System.out.println(Arrays.toString(arr));
-                i = -1;
+            for(int j = i+1 ; j < arr.length ; j++){
+                if(arr[j] < arr[i]){
+                    int temp = arr[j] ;
+                    arr[j] = arr[i] ;
+                    arr[i] = temp ;
+                 }
             }
         }
         System.out.println(Arrays.toString(arr));
