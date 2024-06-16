99d9c17452344e690ff3569f8c66955f27624ba6
Chetna1405 <gargchetna11@gmail.com>
2024-06-16 12:44:35 +0530

    ..

--- PracticeSet/GreedyAlgorithm/AssignCookies.java
+++ PracticeSet/GreedyAlgorithm/AssignCookies.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class AssignCookies {
     public static void main(String[] args) {
         int[] g = {3,4,2,1,5};
-        int[] s = {1,3,2,4,5};
+        int[] s = {1,1,1};
         System.out.println(findContentChildren(g,s));
     }
     public static int findContentChildren(int[] g, int[] s) {
@@ -13,17 +13,13 @@ public class AssignCookies {
         Arrays.sort(s);
         int left = 0 ;
         int right = 0 ;
-        int c = 0 ;
-
         while(left < g.length && right < s.length ){
-            if(g[left] <= s[right]){
-                c++;
+            if(g[left] <= s[right]) {
                 left++;
-                right++;
-            }else{
-                right++;
             }
+                right++;
+
         }
-        return c ;
+        return left ;
     }
 }
